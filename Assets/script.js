b9849d02ecb28d49d376b64cc747456e011ac120
Ben Elliott <benelliott111@gmail.com>
2021-02-10 10:11:19 -0500

    final version of application

--- Assets/script.js
+++ Assets/script.js
@@ -4,24 +4,65 @@ let weatherCurrent = ""
 let city = ""
 var atlanta = document.querySelector('#city').innerHTML
 
-async function currentWeather(cityName){
-    event.preventDefault()
+var favsList = []
+if (localStorage.favsList) {
+    favsList = JSON.parse(localStorage.favsList)
+}
+
+for(let i = 0; i < favsList.length; i++){
+    document.querySelector('#favourites').innerHTML +=`<div class="mb-3">
+        <button class="btn btn-primary" type="button" id="${favsList[i]}" onclick='loadFavourites("${favsList[i]}")'>${favsList[i]}</button>
+        </div>`
+}
+
+
+function loadFavourites(city){
+    currentWeather(city)
+}
+
+function runCity(){
     city = document.querySelector('#cityName').value
-    cityStore = city
+    currentWeather(city)
+    var favouriteCity = document.querySelector('#cityName').value
+    console.log(favouriteCity)
+    if(favsList.includes(favouriteCity) !== true){
+    favsList.push(favouriteCity)
+    localStorage.favsList = JSON.stringify(favsList)
+    document.querySelector('#favourites').innerHTML +=`<div class="mb-3">
+        <button class="btn btn-primary" type="button" id="${favouriteCity}" onclick="loadFavourites('${favouriteCity}')">${favouriteCity}</button>
+        </div>`
+        
+}
+    
+}
+
+
+
+async function currentWeather(city){
     document.querySelector('#forecastCard').innerHTML = ""
     // fetch weather for given city
     weather = await fetch (`http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=fe880404e0472c9c21ec6d8e606cea67`).then(r => r.json())
     weatherCurrent = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${weather.coord.lat}&lon=${weather.coord.lon}&units=metric&appid=fe880404e0472c9c21ec6d8e606cea67`).then(r => r.json())
     console.log(weather)
     //Fills in current weather 
+    document.querySelector('#currentIcon').src = `https://openweathermap.org/img/w/${weatherCurrent.current.weather[0].icon}.png`
     document.querySelector('#currentCity').innerHTML = `${city}, ${currentDay} `;
     document.querySelector('#currentTemp').innerHTML = `Temperature: ${weather.main.temp}C`
     document.querySelector('#currentHumidity').innerHTML = `Humidity: ${weather.main.humidity}%`
     document.querySelector('#currentWind').innerHTML = `Wind Speed: ${weather.wind.speed}km/hr`
-    document.querySelector('#currentUV').innerHTML = `UV Index: ${weatherCurrent.current.uvi}`
-    document.querySelector('#favourites').innerHTML +=`<div class="mb-3">
-        <button onclick="currentWeather()">${city}</button>
-        </div>`
+    document.querySelector('#currentUV').innerHTML = `${weatherCurrent.current.uvi}`
+    if (weatherCurrent.current.uvi <= 2){
+        document.querySelector('#currentUV').style.backgroundColor = 'green';
+    } else if (weatherCurrent.current.uvi > 2 && weatherCurrent.current.uvi <= 5){
+        document.querySelector('#currentUV').style.backgroundColor = 'yellow';
+    } else if (weatherCurrent.current.uvi > 5 && weatherCurrent.current.uvi <= 7){
+        document.querySelector('#currentUV').style.backgroundColor = 'orange';
+    } else if (weatherCurrent.current.uvi > 7 && weatherCurrent.current.uvi <= 10){
+        document.querySelector('#currentUV').style.backgroundColor = 'red';
+    } else {
+        document.querySelector('#currentUV').style.backgroundColor = 'purple';
+    }
+    
 
     //Generates and fills in 5 day forecast 
     for (var i = 0; i < 5; i++ ){
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather-API
+
+Upon entering a city into the input and clicking the button, the application will load the weather for the day, and the five day forecast below. The UV index is colour coded based on number. 
+
+When a city is searched, it is saved to local storage and a button is added below the search bar that will allow for renavigation to the city weather page. 
+
+The webpage can be accessed here: https://benelliott111.github.io/Weather-API/
+
